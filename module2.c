64d844bef3f02e49c303e86584f2fc88aa824cbb
cvancory1 <cvancory1@gmail.com>
2021-11-20 17:16:24 -0500

    calcs the length for matrixA length

--- module2.c
+++ module2.c
@@ -54,10 +54,15 @@ void everyonePrint(int rank , char * str , int *  arr ){
   printf("Rank =%d %s=%d\n", rank, str, arr[rank]);
 }
 
+
+
+
+//  QUESTION - DOES THIS NEED TO HAVE THE GLBAL PAPER ID ALSO? 
+// is it a int * ?? because it is just holding either a 0 or 1 
 typedef struct rows {
   int length;
-  int* data;
-} row;
+  int * data; // Holds the global index from db that this paper cites 
+} rows;
 
 int main(int argc, char** argv) {
   MPI_Init(&argc, &argv);
@@ -68,19 +73,42 @@ int main(int argc, char** argv) {
   MPI_Comm_rank(world, &rank);
   MPI_Get_processor_name(name, &nameLen);
 
+
+  // opens the citation file 
   FILE * fp;
-  fp = fopen("testCitations.txt", "r");
+  // fp = fopen("testCitations.txt", "r");
+  // int TOTALPAPERS = 50; 
+
+  int TOTALPAPERS = 1354753; 
+  fp = fopen("arxiv-citations.txt", "r");
+
 
   if(fp== NULL ) printf("ERROR opening file ");
 
-  int TOTALPAPERS = 50; 
 
-   // counts - number of bytes each proc will read 
-  int* sendcnt = malloc(worldSize * sizeof(int));
+
+  // tracks the amount of papers each proc will read in
   int * localPapercount = findCounts(TOTALPAPERS, worldSize); 
+  
+  //based on localpapercount , calc the bytes ( ie cant divide bytes / worldsize since the file wouldnt be parsed correctly in every case)
+  int* sendcnt = malloc(worldSize * sizeof(int));
 
 
+  // initialize & allocate MatrixA for power method 
+  /* assume there are at least 10 citations and allocate, if more than reallocate later */ 
 
+  rows matrixA [TOTALPAPERS];
+  int MALLOCEDSIZE = 10 ; 
+  if(rank == ROOT) {
+    for(int i =0; i< TOTALPAPERS ; i++){
+      matrixA[i].data = malloc(MALLOCEDSIZE * sizeof(int));
+      matrixA[i].length  = 0;  
+    } 
+
+  }
+  
+
+  /* does calcs for number of bytes every proc needs to read in based on even dividsion of papers */ 
   if(rank == ROOT){
     char * line= NULL; 
     int numread;
@@ -90,32 +118,54 @@ int main(int argc, char** argv) {
     for (int i = 0; i < worldSize; i++) sendcnt[i] = 0; // initalize the sendcounts = 0 
 
     int citedCount = 0;  // counts the len of "rows" array
-    size_t len = 0;
+    size_t len = 0; // used by getline function (gets returned a power of 2 corresponding to bytes read)
 
+    int matrixA_it = 0; // traverse through A arrary
+    int checkCitations = 0; // ensures the name being read in is a cited paper and an actual paperID
     while ((numread = getline(&line, &len, fp)) != -1) {
-      sendcnt[i] += numread;
-      // line[len-1] = '*';
+      sendcnt[i] += numread; // tracks the total amount of bytes
       // printf("line: %s count: %zu\n", line, len);
+
+        if(checkCitations == 1 && line[0] != '+'){
+          matrixA[matrixA_it].length++;
+          // printf("matrixA_it =%d %s\n", matrixA_it , line);
+          // TODO - Query bevans db here
+
+          // more than 10 citations , need to realloc the data array
+          if(matrixA[matrixA_it].length > MALLOCEDSIZE ){
+            int newSize = matrixA[matrixA_it].length * 2; // curent length * 2, first resize occurs after 10 citations
+            matrixA[matrixA_it].data = realloc(newSize * sizeof(int));
+
+          }
+
+        }else if (checkCitations == 1 && line[0] == '+' ) {
+          checkCitations =0; 
+        }
 
         int length = strlen(line);
         // printf("length=%d \n", length);
-        printf("line[len-1]=%c  line[len] =%c ",line[length-2] , line[length-1] );
-        if(line[length-1] == '-' && line[length] == '\n'){
-          citedCount++;
+        // printf("line[len-1]=%c  line[len] =%c ",line[length-2] , line[length-1] );
+        if(line[length-2] == '-' && line[length-1] == '\n'){
+          
+          // citedCount++;
+          // matrixA[curPaperCount].length++;
+          // // printf("ENTER \n");
+          checkCitations = 1; // the next name you read in is a citation
 
         }
 
-        // +, indicates a new paper in the list 
+
+        // +,  a new paper is being read from the file
         if(line[0] == '+' ){
           curPaperCount++;
-          if(curPaperCount == localPapercount[i] ){
+          matrixA_it++;
 
-            printf("citedCount=%d \n", citedCount);
+          if(curPaperCount == localPapercount[i] ){
+            // printf("citedCount=%d \n", citedCount);
             citedCount =0; // resets the cited count
 
             i++;
             curPaperCount=0;
-
           }
 
         }
@@ -124,15 +174,22 @@ int main(int argc, char** argv) {
         line =NULL; 
         len =0; 
 
-
-
     }
 
   }
+
+  // all nodes know how many to read in from the file 
   MPI_Bcast(sendcnt, worldSize, MPI_INT, ROOT, world);
-  everyonePrint(rank, "count", sendcnt);
+  // everyonePrint(rank, "count", sendcnt);
 
-
+  
+  
+  // printing results 
+  if(rank == ROOT) {
+    for(int i =0; i< TOTALPAPERS ; i++){
+      printf("i=%d matrixA.length=%d\n", i, matrixA[i].length );
+    } 
+  }
 
 
 
